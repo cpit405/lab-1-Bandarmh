46e2cd9bf3db8f1c47a42a4edd3ce596e1830360
bandar <bandar2996@gmail.com>
2023-05-17 23:22:55 +0300

    init

---
+++ Lab8-React/my-react-app/src/Components/SearchBox.js
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import './SearchBox.CSS'
+
+export default function SearchBox(){
+    const[CityName, setCityName] = useState("");
+    const[forecasts, setforecasts] = useState([]);
+
+    const handleKeyDown = (event) => {
+        if(event.key === 'Enter' && CityName.trim().length >2){
+            const URL = "http://api.weatherapi.com/v1/current.json?key=51236aca09894253905162316231705&q=" + CityName + "&aqi=n";
+            try{ 
+                const respons = await fetch(URL, {method: 'GET', headers: {Accept: 'application/json'}});
+                if(!response.ok){
+                throw new Error (`Error failed to send an HTTP Get Request: ${response.status}`);
+            }
+            const dataa = await response.json();
+            console.log(data.forecast.forecastday);
+            setforecasts(data.forecast.forecastday);
+        }
+        catch(err){
+            console.error(err);
+        }
+    }
+
+    function handleChange(event){
+        setCityName(event.target.value);
+    }
+
+    return(
+    <div>
+        <input type="text"
+        className="SearchBox"
+        placeholder="Enter city name"
+        onChange={handleChange}
+        onKeyDown={handleKeyDown}
+        />
+        <h3> Wheather for {CityName} </h3>
+        <ul>
+            {forecasts.map(function(forecast, i){
+                return(
+                    <li key={i}>
+                        <span>{forecast.data}</span>
+                        <span>{forecast.data.maxtemp_c}</span>
+                    </li>
+                )
+            })}
+        </ul>
+     </div>
+    )
+}
--- Lab8-React/my-react-app/src/pages/Home.js
+++ Lab8-React/my-react-app/src/pages/Home.js
@@ -1,9 +1,12 @@
+import SearchBox from "../Components/SearchBox";
+
 export default function Home(){
     return(
         <div>
             
             <h1>Home</h1>
-            
+            <SearchBox/>
+
         </div>
     )
 }
