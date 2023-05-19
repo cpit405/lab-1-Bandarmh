cfffcabfd13f1bfa20c7cbd0340adf1c31ba6a01
bandar <bandar2996@gmail.com>
2023-05-19 04:53:03 +0300

    init

--- react-weather-app-using-fetch-API-and-useState-hook/react-weather-app-main/src/SearchBox.js
+++ react-weather-app-using-fetch-API-and-useState-hook/react-weather-app-main/src/SearchBox.js
@@ -3,11 +3,25 @@ import './SearchBox.css';
 
 export default function SearchBox() {
     const [cityName, setCityName] = useState('');
+    const [forecasts, setForecast] = useState([]);
 
-
-    function handleKeyDown(event) {
-        if (event.key === 'Enter') {
-            console.log('Did hit Enter key');
+    //without theuseEffect hook
+    const handleKeyDown = async(event) =>{
+        if (event.key === 'Enter' && cityName().trim().length>2) {
+            const URL = "http://api.weatherapi.com/v1/current.json?key=51236aca09894253905162316231705&q=" + CityName + "&aqi=n";
+            try{
+                const respons = await fetch(URL, {mothod: 'GET', header: {Accept: 'application/json'}});
+                if(!Response.ok){
+                    throw new Error(`Error faild to send an HTTP GET request: ${response.status}`);
+                }
+                const data = await respons.json();
+                console.log(data.forecasts.forecastsday);
+                setForecast(data.forecasts.forecastsday)
+            }
+            catch(err){
+                console.error(err);
+            }
+            
         }
     }
 
@@ -23,7 +37,18 @@ export default function SearchBox() {
                 onChange={handleChange}
                 onKeyDown={handleKeyDown}
             />
-            <h2>You've typed: {cityName} </h2>
+            <ul>
+            {forecasts.map(function(pforecast, i){
+                return( 
+                <li key ={i}>
+                    <span>{forecasts.data}</span>
+                    <span>{forecasts.data.maxtemp_c}</span>
+                     </li>
+
+                )
+            })
+        }
+           </ul>
         </div>
     )
 }
