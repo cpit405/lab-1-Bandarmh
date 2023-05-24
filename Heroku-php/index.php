ff8be1a5b31222edb04ca763012dd429eb35c678
bandar <bandar2996@gmail.com>
2023-05-25 00:44:43 +0300

    init

---
+++ Heroku-php/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en,ar">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title> PHP test </title>
+</head>
+
+   <?php 
+   echo '<p> Hello php  </p>';
+   ?>
+
+   </body>
+ </html>
