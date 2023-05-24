6119d755b7ff7aeea19ad4225b38d5889e2dcaaf
bandar <bandar2996@gmail.com>
2023-05-25 02:17:12 +0300

    init

--- Lab-9-php-api/index.php
+++ Lab-9-php-api/index.php
@@ -3,7 +3,7 @@
 require_once('./db_connections.php');
 
 header("Content-Type:application/json");
-if ($_SERVER["REQUEST_METHOD"] == "GET"{
+if ($_SERVER["REQUEST_METHOD"] == "GET"){
     require('./get_todos.php');
     get_all_todos();
 }
