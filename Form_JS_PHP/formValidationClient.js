d2a77ab4b80607d43de8a3ac940759efab1d1875
bandar <bandar2996@gmail.com>
2023-06-22 11:06:06 +0300

    init

--- Form_JS_PHP/formValidationClient.js
+++ Form_JS_PHP/formValidationClient.js
@@ -1,32 +1,26 @@
 function validateForm(event) {
-    let Nname = document.forms ["myForm" ] ["uname"].value;
-    let email = document.forms ["myForm"] ["email"].value;
-    let comment = document.forms ["myForm"] ["ucomment"].value;
-
-    let errorMessageElem = document.getElementById ("error-message");
-    errorMessageElem.innerHTML = "";
-    let isValid = true;
-
-    //name validation
-    const userNameRegExp = /^{8, 32}$/;
-    if (Nname == "" || !userNameRegExp.test(Nname)) {
-    errorMessageElem.innerHTML += "<p>Invalid user name. User name must be between 8 and 32 charecter</p>";
-    isValid = false;
+    const nameInput = document.getElementById('userNameInput');
+    const emailInput = document.getElementById('emailInput');
+    const commentInput = document.getElementById('comment');
+    const errorMessage = document.getElementById('error-message');
+  
+    if (nameInput.value.split(' ').length < 2) {
+      errorMessage.innerText = 'Please enter your first and last name.';
+      event.preventDefault();
+      return;
     }
-    //comment validation
-    const commentRegExp = /^{0, 150}$/;
-    if (comment == "" || !userNameRegExp.test(comment)) {
-    errorMessageElem.innerHTML += "<p>Invalid This comment box is reqierd</p>";
-    isValid = false;
+  
+    if (!emailInput.checkValidity()) {
+      errorMessage.innerText = 'Please enter a valid email address.';
+      event.preventDefault();
+      return;
     }
-    // Email regular expression pattern (RFC 5322 Official Standard)
-    const regExpPattern = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
-    if (email == "" || !regExpPattern.test (email)) {
-    errorMessageElem.innerHTML += "«p>Invalid email. A valid email address is required.</p>"
-    isValid = false;
+  
+    if (commentInput.value.length > 150) {
+      errorMessage.innerText = 'Please limit your comment to 150 characters or less.';
+      event.preventDefault();
+      return;
     }
-    //If validation has failed for any input field, do not submit the form
-    if(!isValid) {
-    event.preventDefault();
-    }
-}
+  
+    errorMessage.innerText = '';
+  }
--- Form_JS_PHP/formValidationServer.php
+++ Form_JS_PHP/formValidationServer.php
@@ -1,42 +1,27 @@
 <?php
- if ($_SERVER['REQUEST_METHOD'] != 'POST' ){
 
-header ($_SERVER ["SERVER_PROTOCOL"]." 405 Method Not Allowed", true, 405);
-die ("<h1>Error: 405 Method Not Allowed</h1>");
- }
 
- // Clean the input (remove extra whitespaces
- $Nname = trim($_POST ["uname"]) ;
- $email = trim($_POST(["email"]));
- $comment = trim($_POST(["ucomment"]));
+$name = $_POST['uname'];
+$email = $_POST['email'];
+$comment = $_POST['comment'];
 
- if (empty($Nname) || empty($email) || empty($comment)) {
+// Validate Name field
+if (empty($name) || strlen(trim($name)) < 2) {
+  die('Error: Please enter your first and last name.');
+}
 
-   echo "<p>Error: User name, email, and password data are required. </p>" ;
+// Validate Email field
+if (empty($email) || !filter_var($email, FILTER_VALIDATE_EMAIL)) {
+  die('Error: Please enter a valid email address.');
+}
 
-return;
- }
+// Validate Comment field
+if (empty($comment) || strlen(trim($comment)) > 150) {
+  die('Error: Please limit your comment to 150 characters or less.');
+}
 
- //name validation
- $unamelength = strlen($Nname);
-  if(preg_match($Nname)) {
-echo "<p>name is valid</p>";
-  }else {
-echo "<p>Error: Invalid user name. User name must be between 8 and 32 characters and may contal</p>";
- }
+// If all fields are valid, send an email or save the data to a database
+// For example:
 
- //comment validation
- $unamelength = strlen($comment);
-  if(preg_match($comment)) {
-echo "<p>comment is valid</p>";
-  }else {
-echo "<p>Error: Invalid writen must be at most 150 charecter</p>";
- }
-
-// Email validation
-  if(filter_var($email, FILTER_VALIDATE_EMAIL)) {
-    echo "<p> Email address is valid. </P>";
-   } else {
-echo "<p>Error: Invalid email address.</p>";
-   }
-   ?>
+echo 'Thank you for your submission!';
+?>
